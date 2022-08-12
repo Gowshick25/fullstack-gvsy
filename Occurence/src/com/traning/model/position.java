a30027f8653a7667100310cb0d3fe2869845d34e
Gowshick25 <103167849+Gowshick25@users.noreply.github.com>
2022-08-12 14:26:46 +0530

    Add files via upload

---
+++ Occurence/src/com/traning/model/position.java
@@ -0,0 +1,29 @@
+package com.traning.model;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class position {
+	public static void main(String []args) {
+	
+	Map<Integer, ArrayList> value= new TreeMap<Integer, ArrayList>();
+	
+	ArrayList<Integer> list = null;
+	
+	
+	int arr[]= {};
+	
+	for (int i=0; i<arr.length;i++)
+	{
+		if (!(value.containsKey(arr[i])))
+		{
+			list= new ArrayList<>();
+			
+			
+		}
+	}
+	}
+}
+	
+
